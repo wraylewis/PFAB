e16d9866fced4e49f97fed4b9856ec4a13ddf940
ray lewis <ray_lewis@me.com>
2014-08-22 21:44:55 -0500

    first commit
    my first PFAB assignment!

---
+++ Homework.py
@@ -0,0 +1,20 @@
+import math
+first = "Ray"
+last = "Lewis"
+age = 70
+print first, last, age
+
+sideA = 12.55
+sideB = 17.85
+sideC = math.sqrt(sideA**2 + sideB **2)
+print sideC
+
+operand1 = 95
+operand2 = 64.5
+
+print operand1,"+", operand2, "=", (operand1 + operand2)
+print "95 - 64.5=", (operand1 - operand2)
+print "95 * 64.5=", (operand1 * operand2)
+print "95 / 64.5=", (operand1 / operand2)
+print "95 % 64.5=", (operand1 % operand2)
+
